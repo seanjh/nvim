99ff033b70621f37d71325ebe3a6917e7c06bf94
Sean Herman <sherman1@rbi.com>
2025-01-24 09:13:13 -0500

    nits

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,72 +1,72 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = { "BufWritePre" },
-        cmd = { "ConformInfo" },
-        opts = {
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_fallback = true,
-            },
-            formatters_by_ft = {
-                nix = { "nixpkgs-fmt" },
-                lua = { "stylua" },
-                javascript = { { "prettierd" } },
-                javascriptreact = { { "prettierd" } },
-                typescript = { { "prettierd" } },
-                typescriptreact = { { "prettierd" } },
-                json = { "prettierd" },
-                jsonc = { "prettierd" },
-                yaml = { "prettierd" },
-                markdown = { "prettierd" },
-                ["markdown.mdx"] = { "prettierd" },
-                css = { "prettierd" },
-                scss = { "prettierd" },
-                html = { "prettierd" },
-            },
-            -- Configure formatters
-            formatters = {
-                eslint_d = {
-                    -- Only run eslint if we find a config file in the project
-                    condition = function(ctx)
-                        return vim.fs.find({
-                            ".eslintrc.js",
-                            ".eslintrc.cjs",
-                            ".eslintrc.yaml",
-                            ".eslintrc.yml",
-                            ".eslintrc.json",
-                            "eslint.config.js",
-                            "package.json",
-                        }, { path = ctx.filename, upward = true })[1] ~= nil
-                    end,
-                },
-                ["nixpkgs-fmt"] = {
-                    command = "nixpkgs-fmt",
-                    stdin = true,
-                },
-                prettierd = {
-                    -- This tells conform to only run prettier when it finds a config file
-                    condition = function(ctx)
-                        return vim.fs.find({
-                            ".prettierrc",
-                            ".prettierrc.js",
-                            ".prettierrc.json",
-                            ".prettierrc.yml",
-                            ".prettierrc.yaml",
-                            ".prettierrc.json5",
-                            ".prettierrc.cjs",
-                            "prettier.config.js",
-                            "prettier.config.cjs",
-                            "prettier.config.mjs",
-                            "package.json",
-                        }, { path = ctx.filename, upward = true })[1] ~= nil
-                    end,
-                },
-                stylua = {
-                    command = "stylua",
-                    args = { "--search-parent-directories", "--stdin-filepath", "$FILENAME", "-" },
-                },
-            },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+      -- format_on_save = {
+      --     timeout_ms = 500,
+      --     lsp_fallback = true,
+      -- },
+      formatters_by_ft = {
+        nix = { "nixpkgs-fmt" },
+        lua = { "stylua" },
+        javascript = { { "prettierd" } },
+        javascriptreact = { { "prettierd" } },
+        typescript = { { "prettierd" } },
+        typescriptreact = { { "prettierd" } },
+        json = { "prettierd" },
+        jsonc = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        ["markdown.mdx"] = { "prettierd" },
+        css = { "prettierd" },
+        scss = { "prettierd" },
+        html = { "prettierd" },
+      },
+      -- Configure formatters
+      formatters = {
+        eslint_d = {
+          -- Only run eslint if we find a config file in the project
+          condition = function(ctx)
+            return vim.fs.find({
+              ".eslintrc.js",
+              ".eslintrc.cjs",
+              ".eslintrc.yaml",
+              ".eslintrc.yml",
+              ".eslintrc.json",
+              "eslint.config.js",
+              "package.json",
+            }, { path = ctx.filename, upward = true })[1] ~= nil
+          end,
         },
+        ["nixpkgs-fmt"] = {
+          command = "nixpkgs-fmt",
+          stdin = true,
+        },
+        prettierd = {
+          -- This tells conform to only run prettier when it finds a config file
+          condition = function(ctx)
+            return vim.fs.find({
+              ".prettierrc",
+              ".prettierrc.js",
+              ".prettierrc.json",
+              ".prettierrc.yml",
+              ".prettierrc.yaml",
+              ".prettierrc.json5",
+              ".prettierrc.cjs",
+              "prettier.config.js",
+              "prettier.config.cjs",
+              "prettier.config.mjs",
+              "package.json",
+            }, { path = ctx.filename, upward = true })[1] ~= nil
+          end,
+        },
+        stylua = {
+          command = "stylua",
+          args = { "--search-parent-directories", "--stdin-filepath", "$FILENAME", "-" },
+        },
+      },
     },
+  },
 }
--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,17 +1,21 @@
+if vim.fn.executable("ollama") ~= 1 then
+  return {}
+end
+
 return {
-    "huggingface/llm.nvim",
-    opts = {
-        model = "codellama:13b",
-        backend = "ollama",
-        url = "http://localhost:11434",
-        request_body = {
-            -- Modelfile options for the model you use
-            options = {
-                temperature = 0.2,
-                top_p = 0.95,
-            },
-        },
-        enable_suggestions_on_startup = true,
-        enable_suggestions_on_files = { "*.py", "*.ts", "*.go", "*.lua" },
+  "huggingface/llm.nvim",
+  opts = {
+    model = "codellama:13b",
+    backend = "ollama",
+    url = "http://localhost:11434",
+    request_body = {
+      -- Modelfile options for the model you use
+      options = {
+        temperature = 0.2,
+        top_p = 0.95,
+      },
     },
+    enable_suggestions_on_startup = true,
+    enable_suggestions_on_files = { "*.py", "*.ts", "*.go", "*.lua" },
+  },
 }
