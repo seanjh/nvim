107ae0ee76865728922c9f225131bab4cadf788c
Sean Herman <seanherman@gmail.com>
2025-01-26 18:35:48 -0500

    LazyVim extras (#5)
    Co-authored-by: Sean Herman <sherman1@rbi.com>

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,21 +1,17 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    config = function()
-      require("neo-tree").setup({
-        filesystem = {
-          filtered_items = {
-            hide_dotfiles = false,
-            hide_gitignored = true,
-            always_show = {
-              ".aws",
-            },
-            always_show_by_pattern = {
-              ".env*",
-            },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          always_show = { ".aws", ".circleci", ".github" },
+          always_show_by_pattern = {
+            ".env*",
           },
         },
-      })
-    end,
+      },
+    },
   },
 }
