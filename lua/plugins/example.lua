fe59dc3d3c2b2cbde1db183b7c973dcc206b7b0d
Sean Herman <seanherman@gmail.com>
2025-01-31 08:44:03 -0500

    formatters rework (#6)
    - Cleanup conform configs
    - re-enable avante
    
    ---------
    
    Co-authored-by: Sean Herman <sherman1@rbi.com>

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,7 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
+local enabled = false
 -- stylua: ignore
-if true then return {} end
+if not enabled then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
