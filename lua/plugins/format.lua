9cd5c9f0151008693f8651f4d2a98185ef6224b6
Sean Herman <seanjh@hey.com>
2025-07-14 14:56:50 -0400

    erb_lint

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -3,6 +3,13 @@ return {
     "stevearc/conform.nvim",
     opts = function()
       return {
+        default_format_opts = {
+          timeout_ms = 5000,
+          async = false, -- not recommended to change
+          quiet = false, -- not recommended to change
+          lsp_format = "fallback", -- not recommended to change
+        },
+
         formatters_by_ft = {
           nix = { "nixfmt" },
           lua = { "stylua" },
@@ -17,8 +24,16 @@ return {
           ["markdown.mdx"] = { "markdownlint" },
           python = { "ruff_format" },
           ruby = { "standardrb" },
+          eruby = { "erb_lint" },
         },
-        formatters = {},
+        formatters = {
+          erb_lint = {
+            stdin = false,
+            tmpfile_format = ".conform.$RANDOM.$FILENAME",
+            command = "bundle",
+            args = { "exec", "erb_lint", "--autocorrect", "$FILENAME" },
+          },
+        },
       }
     end,
   },
