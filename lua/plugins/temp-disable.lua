248f7cfe91559054afbf679cb318565a5ea08238
Sean Herman <seanjh@hey.com>
2026-01-30 09:29:57 -0500

    save

---
+++ lua/plugins/temp-disable.lua
@@ -0,0 +1,5 @@
+return {
+  -- { "saghen/blink.cmp", enabled = false },
+  -- { "folke/flash.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false, opts = { cmdline = { enabled = false } } },
+}
