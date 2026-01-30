e64ecc9cce2bc4d9777c251a4088d0c19a3b5908
Sean Herman <seanjh@hey.com>
2026-01-30 09:30:58 -0500

    remove temp

--- lua/plugins/temp-disable.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- { "saghen/blink.cmp", enabled = false },
-  -- { "folke/flash.nvim", enabled = false },
-  -- { "folke/noice.nvim", enabled = false, opts = { cmdline = { enabled = false } } },
-}
--- lua/plugins/treesitter-compiler.lua
+++ lua/plugins/treesitter-compiler.lua
@@ -1,10 +1,8 @@
--- ~/.config/nvim/lua/plugins/treesitter-compiler.lua
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       require("nvim-treesitter.install").compilers = { "clang", "clang++" }
-      -- opts.auto_install = false
       return opts
     end,
   },
